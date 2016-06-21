d1d41d628d88a4d24145b48f07c8644449152bba
Glen Jarvis <glen@glenjarvis.com>
2016-06-21 00:12:21 -0700

    Further parse datetime entries into their own field

--- commit.py
+++ commit.py
@@ -24,6 +24,49 @@ Example raw commit:
  'author': 'Glen Jarvis <glen@glenjarvis.com> 1465164241 -0700'}
 """
 
+import datetime
+import pytz
+
+
+def swap_sign(sign):
+    """Given a sign (+ or -), return opposite"""
+
+    assert sign in ["+", "-"]
+    if sign == "+":
+        return "-"
+    if sign == "-":
+        return "+"
+
+
+def epoch_to_utc(epoch_seconds):
+    """Given seconds form the Epoch, return tz aware UTC datetime"""
+
+    return pytz.utc.localize(
+        datetime.datetime.utcfromtimestamp(float(epoch_seconds))
+    )
+
+
+def zone_from_offset(offset_string):
+    """Given Git offset string, return pytz timezone
+
+    Example:
+    >> offset_string = "-0700"
+    >> time_zone = zone_from_offset(offset)
+    >> type(time_zone)
+    <class 'pytz.tzfile.Etc/GMT+7'>
+
+    Note: The swapped sign "GMT+7" vs "-0700" is intentional:
+
+    https://en.wikipedia.org/wiki/ISO_8601#Time_offsets_from_UTC
+
+    Feedback regarding the philosophy of GMT, UTC, and Zulu here
+    and suggestions for more clarity appreciated.
+    """
+    sign = offset_string[0]
+    hour = offset_string[1:3]
+    return pytz.timezone("Etc/GMT{0}{1}".format(swap_sign(sign),
+                                                int(hour)))
+
 
 class ParsedCommit(dict):
     """Commit Parser used to parse raw commit messages
@@ -34,7 +77,7 @@ class ParsedCommit(dict):
     encountered.
     """
 
-    # PyLint, it's okay that this has so many (22) public methods
+    # PyLint, it's okay that this has so many public methods
     # pylint: disable=R0904
 
     HEADERS_STATE = 1
@@ -119,6 +162,38 @@ class ParsedCommit(dict):
             self.current_state = self.HEADERS_STATE
         self.gpgsig.append(line.strip())
 
+    def update_field_datetime(self, field):
+        """Given field name update post-parsing
+
+        Some fields (i.e., author|committer) contain date-time
+        information. For example:
+        > Glen Jarvis <glen@glenjarvis.com> 1466463764 -0700
+
+        Process field, moving the date time information to its own field
+        and converted the an aware datetime.  For example:
+
+        'author': 'Glen Jarvis <glen@glenjarvis.com>'
+        'author_datetime': datetime.datetime(
+            2016, 6, 20, 16, 2, 44, tzinfo=<StaticTzInfo 'Etc/GMT+7'>)
+        """
+        components = self[field].split()
+        epoch, offset = components[-2:]
+        # Remove time component from field:
+        self[field] = " ".join(components[:-2])
+
+        utc_date_time = epoch_to_utc(epoch)
+        time_zone = zone_from_offset(offset)
+
+        # Add new field {field}_time:
+        self["{0}_datetime".format(field)] =\
+            utc_date_time.astimezone(time_zone)
+
+    def update_timestamps(self):
+        """Update author and committer fields after basic commit is parsed"""
+
+        self.update_field_datetime("author")
+        self.update_field_datetime("committer")
+
     def parse_commit(self):
         """Parse raw commit as given by `git cat-file -p`
 
@@ -142,4 +217,5 @@ class ParsedCommit(dict):
             self["gpgsig"] = "\n".join(self.gpgsig)
 
         self["message"] = "\n".join(self.message)
+        self.update_timestamps()
         return self
